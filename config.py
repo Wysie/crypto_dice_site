ee8edbc3407aec89725142883b97a3fee1fa5282
Soh Yuan Chin <sohyuanchin@gmail.com>
2014-08-07 17:01:10 +0800

    Added config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+port=8888
+debug_mode=True
+sql_host='SQL Endpoint Address'
+sql_port='SQL Port'
+sql_db='SQL DB Name'
+sql_user='SQL Username'
+sql_password='SQL Password'
+coin_host='Coin Daemon RPC Host Address. Typically this would be localhost.'
+coin_port='Coin Daemon RPC Port'
+coin_user='Coin Daemon RPC Username'
+coin_password='Coin Daemon RPC Password'
+walletpassword='Coin Daemon Wallet Password'
